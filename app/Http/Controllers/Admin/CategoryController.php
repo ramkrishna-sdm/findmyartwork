895cb073f4b035e80080605e6702e9e3725e8262
Shambhu <shambhu.thakur@smartdatainc.net>
2019-11-19 23:01:32 +0530

    Manage category,subcategory,style and subject

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,12 +3,40 @@
 namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Repository\CategoryRepository;
 use Validator;
 use Exception;
 use Session;
 
 class CategoryController extends Controller
 {
+    /**
+    * Construction function
+    * @param $request(Array), $categoryRepository(Repository Interface)
+    * @return 
+    *
+    * Created By: Shambhu Thakur
+    * Created At: 19Nov2019 
+    */
+    public function __construct(Request $request, CategoryRepository $categoryRepository)
+    {
+        //$this->middleware('auth');
+        $this->request = $request;
+        $this->categoryRepository = $categoryRepository;
+    }
+    /**
+    * Function To List all Category
+    * @param
+    * @return 
+    *
+    * Created By: Shambhu Thakur
+    * Created At: 19Nov2019 
+    */
+    public function index() {
+        dd('welcome to list');
+    	$templates = $this->templateRepository->getData(['is_deleted'=>'no',  'created_by'=>'admin'],'get',[],0);
+    	return view('backend.templates', compact('templates'));
+    }
      /**
     * Function to render Add Category Page
     * @param
@@ -34,9 +62,8 @@ class CategoryController extends Controller
 			'name' => 'required',
 		]);
     	try{
-            
-            category::create($request->all());
-	    	$save_template = $this->templateRepository->createUpdateData(['id'=> $this->request->id],$this->request->all());
+            $save_category = $this->categoryRepository->createUpdateData(['id'=> $this->request->id],$this->request->all());
+           
 	    	\Session::flash('success_message', "Template Saved Successfully!");
 	    	return redirect('/admin/category');
     	}catch (\Exception $ex){
