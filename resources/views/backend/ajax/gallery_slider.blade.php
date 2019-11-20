b5efa5726f44baf5c7d7fcd0ecc1b7664b2c6e1c
Ram Murthy <ram.murthy@smartdatainc.net>
2019-11-21 03:33:40 +0530

    Artwork Module start

--- resources/views/backend/ajax/gallery_slider.blade.php
+++ resources/views/backend/ajax/gallery_slider.blade.php
@@ -1,23 +1,15 @@
-<div class="modal-dialog modal-lg">
-    <div class="modal-content">
-        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner">
-                @if(count($gallery)>0)
-                @foreach($gallery as $key => $image)
-                <div class="item active">
-                    <img class="img-responsive" src="{{$image->media_url}}" alt="...">
-                </div>
-                @endforeach
-                @endif
-            </div>
-            <!-- Controls -->
-            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left"></span>
-            </a>
-            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right"></span>
-            </a>
+<div id="myGallery" class="carousel slide" data-interval="false">
+    <div class="carousel-inner">
+        @if(count($gallery)>0)
+        @foreach($gallery as $key => $image)
+        <div class="item @if($key == 0) active @endif">
+            <img src="{{$image->media_url}}" alt="item{{$key}}">
         </div>
+        @endforeach
+        @endif
+        <!--end carousel-inner-->
     </div>
+    <!--Begin Previous and Next buttons-->
+    <a class="left carousel-control" href="#myGallery" role="button" data-slide="prev"> <span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#myGallery" role="button" data-slide="next"> <span class="glyphicon glyphicon-chevron-right"></span></a>
+    <!--end carousel-->
 </div>
--- resources/views/backend/artworks.blade.php
+++ resources/views/backend/artworks.blade.php
@@ -53,7 +53,7 @@
                                                     <td><a href="{{url('view_artwork')}}/{{$artwork->id}}">{{$artwork->title}}</a></td>
                                                     <td>{{$artwork->category_detail->name}}</td>
                                                     <td>{{$artwork->sub_category_detail->name}}</td>
-                                                    <td><img src="{{$artwork->artwork_images[0]->media_url}}" class="show_slider" height="50px" width="100px" data-artwork-id="{{$artwork->id}}" data-toggle="modal" data-target=".bs-example-modal-lg"></td>
+                                                    <td><img src="{{$artwork->artwork_images[0]->media_url}}" class="show_slider" height="50px" width="100px" data-artwork-id="{{$artwork->id}}" data-toggle="modal" data-target="#myModal"></td>
                                                     <td class="text-right">
                                                         <a href="{{url('change_artwork_status')}}/{{$artwork->id}}/{{$artwork->is_publised}}/list/{{$artwork->gallery_user_id}}" class="btn btn-danger btn-link btn-sm change_artwork_status" title="@if($artwork->is_publised == 'publish') Un-Publish @else Publish @endif"><i class="fa fa-power-off"></i></a>
                                                     </td>
@@ -73,7 +73,29 @@
 
 
 
-    <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-        
+    <!--begin modal window-->
+<div class="modal fade" id="myModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <h5 class="modal-title" id="exampleModalLabel">Gallery Images</h5>
+            </div>
+            <div class="modal-body">
+                <!--CAROUSEL CODE GOES HERE-->
+
+                <!--end modal-body-->
+            </div>
+            <div class="modal-footer">
+                <button class="btn-sm close" type="button" data-dismiss="modal">Close</button>
+                <!--end modal-footer-->
+            </div>
+            <!--end modal-content-->
+        </div>
+        <!--end modal-dialoge-->
     </div>
+    <!--end myModal-->
+</div>
 @endsection
