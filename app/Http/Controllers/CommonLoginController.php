133bea6c425f93c353f42eb3e73193cf02f3da6a
Ram Murthy <ram.murthy@smartdatainc.net>
2019-12-03 00:09:45 +0530

    Sprint 2

--- app/Http/Controllers/CommonLoginController.php
+++ app/Http/Controllers/CommonLoginController.php
@@ -54,32 +54,32 @@ class CommonLoginController extends Controller
 	  		return redirect()->back()->withErrors($validator);   
 		}else{
 	  
-		   	$userdata = array(
-		        'email' => Input::get('email'),
-		        'password' => Input::get('password'),
+		   $userdata = array(
+	        'email' => Input::get('email'),
+	        'password' => Input::get('password'),
 	        );
-            if(Auth::attempt($userdata)){		
-				$user_role = Auth::user()->role;
-	                
-	            if($user_role == "admin"){
-                	return redirect()->to('/home'); 
-              	}
-	                 
-				if($user_role == "buyer"){
-					return redirect()->to('/buyer/dashboard'); 
-				}
-				     
-				if($user_role == "artist"){
-					return redirect()->to('/artist/dashboard'); 
-				}
-				     
-				if($user_role == "gallery"){
-					return redirect()->to('/gallery/dashboard'); 
-				}
-	                 
-	        }else{
-              return redirect()->back()->with('error', 'User with these credentials is not found!');
-            }
+	            if(Auth::attempt($userdata)){		
+					$user_role = Auth::user()->role;
+		                
+		            if($user_role == "admin"){
+	                	return redirect()->to('/home'); 
+	              	}
+		                 
+					if($user_role == "buyer"){
+						return redirect()->to('/buyer/dashboard'); 
+					}
+					     
+					if($user_role == "artist"){
+						return redirect()->to('/artist/dashboard'); 
+					}
+					     
+					if($user_role == "gallery"){
+						return redirect()->to('/gallery/dashboard'); 
+					}
+		                 
+		        }else{
+	              return redirect()->back()->with('error', 'User with these credentials is not found!');
+	            }
 	    }       
                                                                              
     }
@@ -108,15 +108,15 @@ class CommonLoginController extends Controller
 	 
 	public function createUser($getInfo,$provider){
 
-	$user = User::where('provider_id', $getInfo->id)->first();
-	if (!$user) {
-	      $user = User::create([
-	        'first_name'     => $getInfo->name,
-	        'email'    => $getInfo->email,
-	        'provider' => $provider,
-	        'provider_id' => $getInfo->id
-	     ]);
-	   }
-	   return $user;
- 	}
+		$user = User::where('provider_id', $getInfo->id)->first();
+		if (!$user) {
+		      $user = User::create([
+		        'first_name'     => $getInfo->name,
+		        'email'    => $getInfo->email,
+		        'provider' => $provider,
+		        'provider_id' => $getInfo->id
+		     ]);
+		   }
+		   return $user;
+     	}
 }
