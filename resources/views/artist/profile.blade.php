ec0da4eff829ad12fbffcd81ac7fa9c4163fd985
Pankaj Joshi <pankaj.joshi@smartdatainc.net>
2019-12-11 05:15:30 +0530

    profile done

--- resources/views/artist/profile.blade.php
+++ resources/views/artist/profile.blade.php
@@ -2,6 +2,33 @@
 @section('content')
 <section class="form-box" >
    <div class="container">
+   <div class="message-alert-top">
+                @if(Session::has('validation'))
+                    <div class="alert alert-danger">
+                        <span class="glyphicon glyphicon-ok"></span>
+                            <em> {!! session('success_message') !!}</em>
+                    </div>
+                @endif
+            </div>
+            <div class="message-alert-top">
+                @if(Session::has('success_message'))
+                    <div class="alert alert-success">
+                        <span class="glyphicon glyphicon-ok"></span>
+                            <em> {!! session('success_message') !!}</em>
+                    </div>
+                @endif
+            </div>
+            <div class="message-alert-top">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+            </div>
       <!-- Form Wizard -->
       <form role="form" enctype="multipart/form-data" action="{{url('artist/update_artist')}}" autocomplete="off" method="post">
          @csrf
@@ -15,32 +42,30 @@
                   <span>Browser Me</span>
                   <input class="image-label" name="media_url" type="file" accept="image/*"> 
                </button>
-            
             </div>
 
             <div class="col-12 col-sm-6 col-md-8 form-wizard profile-setting">
-               <h2>Profile Management</h2>
+               <h2 class="text-center">Profile Management</h2>
                @if(!empty($artist))
                <div class="row">
                   <div class="col-12 categorySection">
                      <div class="d-flex justify-content-between cat-sub">
                         <div class="form-group">
                            <label>First Name: <span>*</span></label>
-                           <input type="text" name=" first_name" placeholder="First Name" class="form-control required" value="{{$artist->first_name}}">
-
-                        </div>
+                           <input type="text" name=" first_name" placeholder="First Name" class="form-control" value="{{$artist->first_name}}">
+                       </div>
                         <div class="form-group">
                            <label>Last Name: <span>*</span></label>
-                           <input  type="text" name="last_name" placeholder="Last Name" class="form-control required" value="{{$artist->last_name}}">
+                           <input  type="text" name="last_name" placeholder="Last Name" class="form-control" value="{{$artist->last_name}}">
                         </div>
                      </div>
                      <div class="form-group">
                            <label>Email: <span>*</span></label>
-                           <input  type="email" name="email" placeholder="Email" class="form-control required" value="{{$artist->email}}">
+                           <input  type="email" name="email" placeholder="Email" class="form-control" value="{{$artist->email}}">
                      </div>
                      <div class="form-group">
                            <label>Alias: <span>*</span></label>
-                           <input  type="text" name="user_name" placeholder="Gallery/artist name" class="form-control required" value="{{$artist->user_name}}">
+                           <input  type="text" name="user_name" placeholder="Gallery/artist name" class="form-control" value="{{$artist->user_name}}">
                      </div>
                      <div class="form-group">
                            <label>Biography: <span></span></label>
@@ -50,30 +75,30 @@
                      <div class="d-flex justify-content-between cat-sub">
                         <div class="form-group">
                            <label>Address / Street name: <span>*</span></label>
-                           <input type="text" name="address" placeholder="Address / Street name" class="form-control required" value="{{$artist->address}}">
+                           <input type="text" name="address" placeholder="Address / Street name" class="form-control" value="{{$artist->address}}">
                         </div>
                         <div class="form-group">
 
                            <label>Zip / Postal code: <span>*</span></label>
-                           <input  type="text" name=" postal_code" placeholder="Zip / Postal code" class="form-control required" value="{{$artist->postal_code}}">
+                           <input  type="text" name=" postal_code" placeholder="Zip / Postal code" class="form-control" value="{{$artist->postal_code}}">
 
                         </div>
                      </div>
                       <div class="d-flex justify-content-between cat-sub">
                         <div class="form-group">
                            <label>State: <span>*</span></label>
-                           <input type="text" name="state" placeholder="State" class="form-control required" value="{{$artist->state}}">
+                           <input type="text" name="state" placeholder="State" class="form-control" value="{{$artist->state}}">
                         </div>
                         <div class="form-group">
                            <label>Country: <span>*</span></label>
-                           <input  type="text" name=" country" placeholder="Country" class="form-control required" value="{{$artist->country}}">
+                           <input  type="text" name=" country" placeholder="Country" class="form-control" value="{{$artist->country}}">
                         </div>
                      </div>
                   </div>
 
                </div>
                @endif
-               <div class="form-wizard-buttons">
+               <div class="form-wizard-buttons text-center">
                   <button type="submit" class="btn btn-submit">Submit</button>
                </div>
             </div>
@@ -86,8 +111,7 @@
 
 <section class="form-box">
    <div class="container">
-  
-   <form class="col-md-12" action="{{ url('artist/profile/password') }}" method="POST">
+      <form  action="{{ url('artist/profile/password') }}" method="POST">
          @csrf
          @method('PUT')
          <div class="card">
@@ -111,11 +135,7 @@
                         <div class="form-group">
                               <input type="password" name="old_password" class="form-control" placeholder="Old password" required>
                         </div>
-                        @if ($errors->has('old_password'))
-                              <span class="invalid-feedback" style="display: block;" role="alert">
-                                 <strong>{{ $errors->first('old_password') }}</strong>
-                              </span>
-                        @endif
+                        
                      </div>
                   </div>
                   <div class="row">
@@ -124,11 +144,7 @@
                         <div class="form-group">
                               <input type="password" name="password" class="form-control" placeholder="Password" required>
                         </div>
-                        @if ($errors->has('password'))
-                              <span class="invalid-feedback" style="display: block;" role="alert">
-                                 <strong>{{ $errors->first('password') }}</strong>
-                              </span>
-                        @endif
+                       
                      </div>
                   </div>
                   <div class="row">
@@ -137,18 +153,14 @@
                         <div class="form-group">
                               <input type="password" name="password_confirmation" class="form-control" placeholder="Password Confirmation" required>
                         </div>
-                        @if ($errors->has('password_confirmation'))
-                              <span class="invalid-feedback" style="display: block;" role="alert">
-                                 <strong>{{ $errors->first('password_confirmation') }}</strong>
-                              </span>
-                        @endif
+                        
                      </div>
                   </div>
             </div>
             <div class="card-footer ">
                   <div class="row">
-                     <div class="col-md-12 text-center">
-                        <button type="submit" class="btn btn-info btn-round">{{ __('Save Changes') }}</button>
+                     <div class="col-md-12 text-center form-wizard-buttons">
+                        <button type="submit" class="btn btn-submit">{{ __('Save Changes') }}</button>
                      </div>
                   </div>
             </div>
