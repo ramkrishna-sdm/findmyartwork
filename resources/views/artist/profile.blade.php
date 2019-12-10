216e12ca2a69883cb05201920c46ba21f7fb7872
Ram Murthy <ram.murthy@smartdatainc.net>
2019-12-10 23:12:35 +0530

    artist profile completed

--- resources/views/artist/profile.blade.php
+++ resources/views/artist/profile.blade.php
@@ -5,6 +5,25 @@
       <div class="row">
          <div class="col-md-12 form-wizard">
             <!-- Form Wizard -->
+            <div class="message-alert-top">
+                @if(Session::has('validation'))
+                <div><div class="alert alert-danger"><span class="glyphicon glyphicon-ok"></span><em> {!! session('success_message') !!}</em></div></div>
+                @endif
+                <!-- @if(Session::has('error_message'))
+                <div><div class="alert alert-danger"><em> {!! session('error_message') !!}</em></div></div>
+                @endif -->
+            </div>
+            <div class="message-alert-top">
+                 @if ($errors->any())
+                     <div class="alert alert-danger">
+                         <ul>
+                             @foreach ($errors->all() as $error)
+                                 <li>{{ $error }}</li>
+                             @endforeach
+                         </ul>
+                     </div>
+                 @endif
+             </div>
             <form role="form" enctype="multipart/form-data" action="{{url('artist/update_artist')}}" autocomplete="off" method="post" id="artist-profile-form">
                 @csrf
                <input type="hidden" name="user_type" value="artist">
@@ -18,20 +37,20 @@
                         <div class="d-flex justify-content-between cat-sub">
                            <div class="form-group">
                               <label>First Name: <span>*</span></label>
-                              <input type="text" name=" first_name" placeholder="First Name" class="form-control required" value="{{$artist->first_name}}" id="artist-first_name">
+                              <input type="text" name=" first_name" placeholder="First Name" class="form-control " value="{{$artist->first_name}}" id="artist-first_name">
                            </div>
                            <div class="form-group">
                               <label>Last Name: <span>*</span></label>
-                              <input  type="text" name="last_name" placeholder="Last Name" class="form-control required" value="{{$artist->last_name}}" id="artist-last_name">
+                              <input  type="text" name="last_name" placeholder="Last Name" class="form-control " value="{{$artist->last_name}}" id="artist-last_name">
                            </div>
                         </div>
                         <div class="form-group">
                               <label>Email: <span>*</span></label>
-                              <input  type="email" name="email" placeholder="Email" class="form-control required" value="{{$artist->email}}" id="artist-email">
+                              <input  type="email" name="email" placeholder="Email" class="form-control " value="{{$artist->email}}" id="artist-email">
                         </div>
                         <div class="form-group">
                               <label>Alias: <span>*</span></label>
-                              <input  type="text" name="user_name" placeholder="Gallery/artist name" class="form-control required" value="{{$artist->user_name}}" id="artist-user_name">
+                              <input  type="text" name="user_name" placeholder="Gallery/artist name" class="form-control " value="{{$artist->user_name}}" id="artist-user_name">
                               @if(Session::has('validator')) 
                                 <div class="alert" style="color:red;background-color:none;font-size:12px;">
                                     *{{ Session::get('validator')}}
@@ -45,21 +64,21 @@
                         <div class="d-flex justify-content-between cat-sub">
                            <div class="form-group">
                               <label>Address / Street name: <span>*</span></label>
-                              <input type="text" name="address" placeholder="Address / Street name" class="form-control required" value="{{$artist->address}}" id="artist-address">
+                              <input type="text" name="address" placeholder="Address / Street name" class="form-control " value="{{$artist->address}}" id="artist-address">
                            </div>
                            <div class="form-group">
                               <label>Zip / Postal code: <span>*</span></label>
-                              <input  type="text" name=" postal_code" placeholder="Zip / Postal code" class="form-control required" value="{{$artist->postal_code}}" id="artist-postal_code">
+                              <input  type="text" name=" postal_code" placeholder="Zip / Postal code" class="form-control " value="{{$artist->postal_code}}" id="artist-postal_code">
                            </div>
                         </div>
                          <div class="d-flex justify-content-between cat-sub">
                            <div class="form-group">
                               <label>State: <span>*</span></label>
-                              <input type="text" name="state" placeholder="State" class="form-control required" value="{{$artist->state}}" id="artist-state">
+                              <input type="text" name="state" placeholder="State" class="form-control " value="{{$artist->state}}" id="artist-state">
                            </div>
                            <div class="form-group">
                               <label>Country: <span>*</span></label>
-                              <input  type="text" name=" country" placeholder="Country" class="form-control required" value="{{$artist->country}}" id="artist-country">
+                              <input  type="text" name=" country" placeholder="Country" class="form-control " value="{{$artist->country}}" id="artist-country">
                            </div>
                         </div>
                      </div>
