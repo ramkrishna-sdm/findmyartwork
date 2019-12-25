f2deea75f3cfdd3b8575ddf34f990dd3bb330ebc
Shambhu <shambhu.thakur@smartdatainc.net>
2019-12-25 20:58:17 +0530

    show sign up on checked

--- resources/views/layouts/frontend/comman_footer.blade.php
+++ resources/views/layouts/frontend/comman_footer.blade.php
@@ -13,7 +13,7 @@
         <div class="Links">
           <h3>Support</h3>
           <ul class="navLink">
-            <!-- <li><a href="#">Shipping & Returns</a></li> -->
+            <!-- <li><a href="#">Shipping & Returns</a></li> ----->
             <li><a href="{{url('faq')}}">Help/FAQ</a></li>
             <li><a href="{{url('terms_conditions')}}">Terms of use</a></li>
             <!-- <li><a href="javascript:void(0);">Project Management</a></li> -->
